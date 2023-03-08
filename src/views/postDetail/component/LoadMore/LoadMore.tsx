c1dcad3b0c56e8f10e728ae252f25207de343949
DongMZ1 <915094594@qq.com>
2023-03-08 14:32:30 -0800

    loadmore component finished

--- src/views/postDetail/component/LoadMore/LoadMore.tsx
+++ src/views/postDetail/component/LoadMore/LoadMore.tsx
@@ -1,10 +1,23 @@
-import {View} from 'react-native'
-import {useState} from 'react'
+import { StyleProp, View, ViewStyle, Text, Pressable } from 'react-native'
+import { useState } from 'react'
 import { PostCommentType } from '../../../../types/posts'
 export type LoadMorePropsType<T> = {
     items: T[]
-    renderItem : (item : T) => JSX.Element,
+    renderItem: (item: T) => JSX.Element,
+    style: StyleProp<ViewStyle>
 }
-export const LoadMore = <T,>({items, renderItem}: LoadMorePropsType<T>) => {
-    return <View></View>
+export const LoadMore = <T,>({ items, renderItem, style }: LoadMorePropsType<T>) => {
+    const [showAll, setshowAll] = useState(false)
+    if (items.length === 0) {
+        return null
+    }
+    return <View style={style}>
+
+        {
+            showAll ? items.map(each => renderItem(each)) : <>
+                renderItem(items[0])
+                {items.length > 1 ? <Pressable onPress={() => setshowAll(true)} style={{ paddingLeft: '10%', paddingVertical: 5 }}><Text>Load {items.length - 1} Replies</Text></Pressable> : null}
+            </>
+        }
+    </View>
 }
